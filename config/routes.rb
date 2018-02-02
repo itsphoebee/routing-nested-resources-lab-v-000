f65797a437196dcfd7bcbff5cd8b8b497228c0b3
Phoebe Lau <phoebelau7@gmail.com>
2018-02-02 21:35:54 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :artists, only: [:show] do
     resources :songs, only: [:index, :show]
   end
-  
-  resources :songs
+
+  resources :songs only: [:index, :show, :new, :create, :edit, :update]
 end
